c2260eb25aa746be7e8de213dbe793d5d55eb17c
dxkeac <31731598+dxkeac@users.noreply.github.com>
2022-04-03 22:47:21 +0800

    Update shift_DUP_INV_TRA.py

--- GIAB_data/breakpoint_shift/shift_DUP_INV_TRA.py
+++ GIAB_data/breakpoint_shift/shift_DUP_INV_TRA.py
@@ -2,29 +2,19 @@
 
 import sys
 import random as rd
-#import numpy as np
 from pyfaidx import Fasta
 import fileinput
 import re
 
-# Load reference genome
 fa = Fasta(sys.argv[1])
-#ATCG = np.array(["A", "T", "C", "G"])
-#error_seq = "".join(ATCG[[rd.randint(0,3) for i in range(size)]])
-#error_num = rd.randint(1,3)
-#error_seq = "".join(ATCG[[rd.randint(0,3) for i in range(error_num)]])
-
 size=int(sys.argv[3])
-
 out_file = open(sys.argv[4], 'w')
 
 for line in fileinput.input(sys.argv[2]):
-    # if header, print and got to next line
     if line[0] == '#':
         line = line.rstrip()
-        print(line,sep="\t",file=out_file)
+        print(line)
         continue
-    # else parse variant record
     line = line.rstrip().split('\t')
     chrom = line[0]
     pos = int(line[1])
@@ -37,7 +27,6 @@ for line in fileinput.input(sys.argv[2]):
             ref_seq_num = pos - size
             nuc = fa[str(chrom)][ref_seq_num -1]
             line[3] = nuc.seq
-            #line[4] = nuc.seq + str(alt)[1:]
             line[1] = pos - size
             LEN=int(re.search( r'SVLEN=(\d+)',line[7],re.I).group(1))
             SVLEN = "SVLEN=" + str(LEN)
@@ -48,20 +37,20 @@ for line in fileinput.input(sys.argv[2]):
             ref_seq_num = pos - size
             nuc = fa[str(chrom)][ref_seq_num -1]
             line[3] = nuc.seq
-            #line[4] = nuc.seq + str(alt)[1:]
             line[1] = pos - size
-            chr2=re.search( r'CHR2=(\d+)',line[7],re.I).group(1)
-            #SVLEN = "SVLEN=" + str(LEN)
-            #SVTYPE = "SVTYPE=" + str(svtype)
+            chr2=re.search( r'CHR2=([\w\d]+)',line[7],re.I).group(1)
+            end=re.search( r'END=([\w\d]+)',line[7]).group(1)
             CHR2 = "CHR2=" + str(chr2)
-            END = "END=" + str(pos - size)
-            line[7] = ";".join(["SVTYPE=BND",END,CHR2])
+            END = "END=" + str(int(end) - size)
+            AEND = str(int(end) - size)
+            line[7] = ";".join(["SVTYPE=BND;SVLEN=1",CHR2,END])
+            ALT = re.sub(r':\d+', ":"+AEND, line[4])
+            line[4] = ALT
     else:
         if svtype =="DUP" or svtype =="INV":
             ref_seq_num = pos + size
             nuc = fa[str(chrom)][ref_seq_num -1]
             line[3] = nuc.seq
-            #line[4] = nuc.seq + str(alt)[1:]
             line[1] = pos + size
             LEN=int(re.search( r'SVLEN=(\d+)',line[7],re.I).group(1))
             SVLEN = "SVLEN=" + str(LEN)
@@ -72,14 +61,15 @@ for line in fileinput.input(sys.argv[2]):
             ref_seq_num = pos + size
             nuc = fa[str(chrom)][ref_seq_num -1]
             line[3] = nuc.seq
-            #line[4] = nuc.seq + str(alt)[1:]
             line[1] = pos + size
-            chr2=re.search( r'CHR2=(\d+)',line[7],re.I).group(1)
-            #SVLEN = "SVLEN=" + str(LEN)
-            #SVTYPE = "SVTYPE=" + str(svtype)
+            chr2=re.search( r'CHR2=([\w\d]+)',line[7],re.I).group(1)
+            end=re.search( r'END=([\w\d]+)',line[7]).group(1)
             CHR2 = "CHR2=" + str(chr2)
-            END = "END=" + str(pos + size)
-            line[7] = ";".join(["SVTYPE=BND",END,CHR2])
+            END = "END=" + str(int(end) + size)
+            AEND = str(int(end) + size)
+            line[7] = ";".join(["SVTYPE=BND;SVLEN=1",CHR2,END])
+            ALT = re.sub(r':\d+', ":"+AEND, line[4])
+            line[4] = ALT
     print(line[0],line[1],line[2],line[3],line[4],line[5],line[6],line[7],line[8],line[9],sep="\t",file=out_file)
 out_file.close()
 print("Done......")
